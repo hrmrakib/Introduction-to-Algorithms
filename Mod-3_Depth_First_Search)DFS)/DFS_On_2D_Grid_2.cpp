c98683bfb4a29edfa0e6a75b3deaf7e10a4be046
hrmrakib <hrmrakibs@gmail.com>
2025-09-09 19:45:04 +0600

    DFS on 2D Grid

--- Mod-3_Depth_First_Search)DFS)/DFS_On_2D_Grid_2.cpp
+++ Mod-3_Depth_First_Search)DFS)/DFS_On_2D_Grid_2.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-char grid[1005][1005];
-bool visited[1005][1005];
+
+char grid[105][105];
+bool visited[105][105];
 vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
+int n, m;
+
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+        return false;
+    return true;
+}
 void dfs(int si, int sj)
 {
     cout << si << " " << sj << endl;
     visited[si][sj] = true;
     for (int i = 0; i < 4; i++)
     {
-        int ci, cj;
-        ci = si + dir[i].first;
-        cj = sj + dir[i].second;
-
-        if (!visited[ci][cj] && grid[ci][cj] == '.')
+        int ci = si + dir[i].first;
+        int cj = sj + dir[i].second;
+        if (valid(ci, cj) && !visited[ci][cj])
         {
             dfs(ci, cj);
         }
@@ -23,7 +30,6 @@ void dfs(int si, int sj)
 
 int main()
 {
-    int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
@@ -37,5 +43,6 @@ int main()
     cin >> si >> sj;
     memset(visited, false, sizeof(visited));
     dfs(si, sj);
+
     return 0;
 }
