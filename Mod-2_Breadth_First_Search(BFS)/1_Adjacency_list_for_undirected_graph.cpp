ea197864f91f16f878b2e3d2c7e93bc3d5b0a1bb
hrmrakib <hrmrakibs@gmail.com>
2025-10-03 11:38:44 +0600

    Add c++ code faster line

--- Mod-2_Breadth_First_Search(BFS)/1_Adjacency_list_for_undirected_graph.cpp
+++ Mod-2_Breadth_First_Search(BFS)/1_Adjacency_list_for_undirected_graph.cpp
@@ -1,23 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n, e;
     cin >> n >> e;
 
     int a[n];
     vector<int> adj_list[n]; // array of vector
 
-    while(e--) {
+    while (e--)
+    {
         int a, b;
         cin >> a >> b;
         adj_list[a].push_back(b);
         adj_list[b].push_back(a); // undirected graph
     }
- 
-    for(int i = 0; i < n; i++) {
-        cout << i << " -> "; 
-        for(int val : adj_list[i]) {
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " -> ";
+        for (int val : adj_list[i])
+        {
             cout << val << " ";
         }
         cout << "\n";
